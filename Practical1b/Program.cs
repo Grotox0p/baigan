400d2624a2dd1fcee63e4b6074d12904e1d8aeee
Grotox0p <93038945+Grotox0p@users.noreply.github.com>
2024-10-06 23:50:13 +0530

    Add project files.

---
+++ Practical1b/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical1b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String");
+            string s = Console.ReadLine();
+            Console.WriteLine(s.ToLower());
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine(s.Contains("More"));
+            Console.WriteLine(s.Length);
+            Console.WriteLine(s.Trim().Length);
+            Console.WriteLine(s.TrimEnd().Length);
+            Console.WriteLine(s.TrimStart().Length);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Practical1c/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical1c
+{
+    struct Student
+    {
+        public int sid;
+        public string sname;
+        public string cname;
+        public int dd, mm, yy;
+        public void display()
+        {
+            Console.WriteLine("Student Name:" + sname);
+            Console.WriteLine("Student ID:" + sname);
+            Console.WriteLine("Student Class:" + sname);
+            Console.WriteLine("Student D.O.B: {0}/{1}/{2}",dd,mm,yy);
+
+        }
+    }
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+                 Console.Write("How many students do you want to enroll");
+                 int n=int.Parse(Console.ReadLine());
+                Student[] allstudents = new Student[n];
+
+                    for (int i=0; i<n;i++)
+                {
+                    allstudents[i] = new Student();
+                    Console.WriteLine("Enter Student Name:");
+                    allstudents[i].sname = Console.ReadLine();
+                    Console.WriteLine("Enter Student Id:");
+                    allstudents[i].sid = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Class name:");
+                    allstudents[i].cname = Console.ReadLine();
+                    Console.WriteLine("Enter Day of Birth:");
+                    allstudents[i].dd=int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Month of Birth:");
+                    allstudents[i].mm = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter year of Birth:");
+                    allstudents[i].yy = int.Parse(Console.ReadLine());
+                }
+            Console.WriteLine("********************Accepted Student Info***********************");
+            for (int i=0; i<n; i++)
+            {
+                Console.WriteLine("====================================================================");
+                allstudents[i].display();
+                Console.WriteLine("====================================================================");
+
+            }
+            Console.ReadKey();
+            }
+    }
+}
